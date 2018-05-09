c2822e16284e64c34ad6f9b00088e96a24cc232b
TheShariana <38940983+TheShariana@users.noreply.github.com>
2018-05-09 09:34:54 +0300

    Delete info.php

--- info.php
+++
@@ -1,9 +0,0 @@
-<?php include_once 'functions/functions.php'; ?>
-<section id="server" class="section server text-center">
-  <h3>SERVER</h3>
-  <?php 
-  	getServerInfo(); 
-  ?>
-  <a class="btn btn-success" role="button" href="https://rust-servers.net/server/118410/vote/" target="_blank" title="Vote">Vote <i class="fa fa-thumbs-up fa-fw"></i></a>
-  <a class="btn btn-primary mx-1 my-1" href="steam://connect/185.36.151.35:28215" title="Connect">Connect <i class="fa fa-gamepad fa-fw" aria-hidden="true"></i></a>
-</section>
