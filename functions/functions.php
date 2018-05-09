ee861ae035f12ea222ba24e79f5a0ad8b56794f5
TheShariana <38940983+TheShariana@users.noreply.github.com>
2018-05-09 09:39:41 +0300

    Added Server info

--- functions/functions.php
+++ functions/functions.php
@@ -1,27 +1,24 @@
 <?php
-
+    $serverName;
     function getServerInfo(){
         $content = file_get_contents("https://api.rust-servers.info/status/598");
         $status  = json_decode($content);
         $content = file_get_contents("https://api.rust-servers.info/info/598");
-        $info  = json_decode($content);
+        $info  = json_decode($content, true);
+
         $content = file_get_contents("https://api.rust-servers.info/update/598");
         $update  = json_decode($content);
+        $image = $info['image'];
+        $serverName = $info['hostname'];
         if($status->status==='Online'){
         	$color = '#00FF00';
         }
         else{
         	$color = '#FF0000';
         }
-        $output = '<p>Status: <span style=\'color:'. $color.'\'>'.$status->status.'</span></p>
-        		<p>IP: <span>'.$info->ip.'</span></p>
-        		<p>Port: <span>'.$info->port.'</span></p>
-        		<p>Players: <span>'.$status->players.' / '.$status->players_max.'</span></p>
-        		<p>Max Players Online: <span>'.$info->players_max_forever.' (1.10.2017 ->)</span></p>
-        		<p>Uptime: <span>'.$status->uptime.'</span></p>
-        		<p>Server Mode: <span>'.$info->server_mode.'</span></p>
-        		<p>Wipe Cycle: <span>'.$info->wipe_cycle.'</span></p>
-        		<p>Updated: <span>'.$update->datetime_full.'</span></p>
-        		'; 
+        
+        $output = '<img src=\''.$image.'\' class=\'server-header img-fluid rounded\'><h5>'.$serverName.'</h5><p>Status: <span style=\'color:'. $color.'\'>'.$status->status.'</span></p>
+            <p>Players: <span>'.$status->players.' / '.$status->players_max.'</span></p>
+            '; 
         echo $output;
     };
